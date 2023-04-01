98955d19f07a25c2f3f70713fd06937eead55630
jycurien <curien.jeanyves@gmail.com>
2023-04-01 14:30:55 +0200

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# laravel-sandbox
+
+A sandbox to play and test things with Laravel, based on Breeze starter kit with TailwindCSS
+
+## Install
+- composer install
+- sail up -d
+- sail artisan migrate --refresh --seed
+- sail npm install
+- sail npm run dev
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="text-center p-16">
+        <h1 class="text-6xl font-bold">Welcome to my awesome blog!</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-200">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+    </div>
+</x-app-layout>
